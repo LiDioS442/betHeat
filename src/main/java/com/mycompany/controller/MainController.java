d86b38056a2ddf4780cad2a981c44b7819f9db9f
Aurélien <aurelien.souvignet@gmail.com>
2017-02-13 23:02:34 +0100

    First Commit

---
+++ src/main/java/com/mycompany/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.mycompany.controller;
+
+/**
+ * Created by Aurel on 13/02/2017.
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value="/",method = RequestMethod.GET)
+    public String homepage(){
+        return "index";
+    }
+}
---
+++ target/classes/static/js/app.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Aurel on 13/02/2017.
+ */
